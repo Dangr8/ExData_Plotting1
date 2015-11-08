0f9273cecf2357cc303c1c8797018563cec0daf0
Dan <darucks@gmail.com>
2015-11-07 18:45:59 -0600

    plot3 added, plot2 corrected typo

---
+++ plot3.R
@@ -0,0 +1,10 @@
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE)
+data2 <- data[grep("\\b1/2/2007\\b|\\b2/2/2007\\b", data$Date),]
+data2$Global_active_power <- as.numeric(data2$Global_active_power)
+alltimes <- strptime(paste(data2$Date, data2$Time, sep = " "),"%d/%m/%Y %H:%M:%S")
+png("plot3.png")
+plot(alltimes, as.numeric(data2$Sub_metering_1), type="l", xlab = "", ylab = "Energy sub metering")
+lines(alltimes, as.numeric(data2$Sub_metering_2), type = "l", col = "red")
+lines(alltimes, as.numeric(data2$Sub_metering_3), type = "l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, lwd = 2)
+dev.off()
